3d48f472ff0ec849d046ca44270381636a1e307d
YovngNegvtive <45307775+YovngNegvtive@users.noreply.github.com>
2018-12-01 10:12:41 +0100

    Update Lesson901.rb

--- Lesson901.rb
+++ Lesson901.rb
@@ -1,3 +1,5 @@
+use_bpm 120
+
 live_loop :jin do
   
   1.times do
@@ -6,14 +8,22 @@ live_loop :jin do
   
   1.times do
     guita
+    1.times do
+      sample
+    end
   end
   
+  1.times do
+    guitai
+  end
+end
+
 
 define :guit do
   play :e3
-  with_fx :echo, mix: 0.3, phase: 0.25 do
+  with_fx :echo, mix: 0.3, phase: 0.50 do
     sample :guit_em9, rate: 0.5
-    sleep 8
+    sleep 4
   end
 end
 
@@ -22,5 +32,23 @@ define :guita do
   with_fx :echo, mix: 0.3, phase: 0.25 do
     sample :guit_em9, rate: 1
     sleep 8
+  end
+end
+sample "D:/IV A Sebastiano Arrigo/samples/TM-88 Snare & Clap 2.wav"
+
+
+
+
+define :guiter do
+  with_fx :echo, mix: 0.3, phase: 0.25 do
+    sleep 8
+  end
+end
+
+define :guitai do
+  play :e3
+  with_fx :echo, mix: 0.9, phase: 1 do
+    sample :guit_em9, rate: 0.25
+    sleep 2
   end
 end
